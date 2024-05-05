ee6baded23049da9df544326ba6b8c35eec33e6a
abdullah01 <arfathossen541@gmail.com>
2024-05-05 08:02:34 +0600

    topic: generic type and ger

--- dist/2.3.js
+++ dist/2.3.js
@@ -1,1 +1,23 @@
 "use strict";
+{
+    //   const RollNumber: number[] = [1, 2, 3, 4, 5];
+    const RollNumber = [1, 2, 3, 4, 5];
+    //   const MentorsName: string[] = ["a", "b", "c", "d", "e", "f"];
+    const MentorsName = ["a", "b", "c", "d", "e", "f"];
+    //   const booleanArray: boolean[] = [true, false];
+    const booleanArray = [true, false];
+    const add = (a, b) => a + b;
+    add(12, 33);
+    const user = [
+        { name: "Arafat", age: 21 },
+        { name: "Abdullah", age: 21 },
+    ];
+    const human = ["Mr. X", "Mr. Y"];
+    //   const userWithId: GenericTupleWithId<number, object, boolean> = [
+    //     11,
+    //     { name: "Arafat", role: "Developer" },
+    //  true,true,
+    //   ];
+    const userWithId = [11, { name: "Arafat", role: "Developer", salary: 10000 }, true];
+    //
+}
---
+++ src/2.3.ts
@@ -0,0 +1,40 @@
+{
+  //Generic type
+
+  type GenericArray<T> = Array<T>;
+
+  //   const RollNumber: number[] = [1, 2, 3, 4, 5];
+  const RollNumber: Array<number> = [1, 2, 3, 4, 5];
+
+  //   const MentorsName: string[] = ["a", "b", "c", "d", "e", "f"];
+  const MentorsName: GenericArray<string> = ["a", "b", "c", "d", "e", "f"];
+
+  //   const booleanArray: boolean[] = [true, false];
+  const booleanArray: Array<boolean> = [true, false];
+  const add = (a: number, b: number) => a + b;
+  add(12, 33);
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    { name: "Arafat", age: 21 },
+    { name: "Abdullah", age: 21 },
+  ];
+
+  // Generic Tuple
+
+  type GenericTuple<X, Y> = [X, Y];
+  const human: GenericTuple<string, string> = ["Mr. X", "Mr. Y"];
+
+  type GenericTupleWithId<A, B, C> = [A, B, C];
+  //   const userWithId: GenericTupleWithId<number, object, boolean> = [
+  //     11,
+  //     { name: "Arafat", role: "Developer" },
+  //  true,true,
+  //   ];
+  const userWithId: GenericTupleWithId<
+    number,
+    { name: string; role: string; salary: number },
+    boolean
+  > = [11, { name: "Arafat", role: "Developer", salary: 10000 }, true];
+
+  //
+}
