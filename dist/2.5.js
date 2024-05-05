ac36165a8a5e9eb161b15c146d33764f548c23db
abdullah01 <arfathossen541@gmail.com>
2024-05-05 19:59:03 +0600

    Generic more powerful features

--- dist/2.5.js
+++ dist/2.5.js
@@ -4,15 +4,37 @@
     const createArray = (param) => {
         return [param];
     };
+    const result1 = createArray("Kabul");
+    //
     const createArrayWithGeneric = (param) => {
         return [param];
     };
-    const result1 = createArray("Habul");
     const result2 = createArrayWithGeneric(12);
     const result3 = createArrayWithGeneric({
         id: 1,
         name: "Habul",
         age: 19,
+    });
+    //   // Function with Tuple
+    const createArrayWithTuple = (param1, param2) => {
+        return [param1, param2];
+    };
+    const result4 = createArrayWithTuple("Kabul", 11);
+    const addCourseToStudent = (student) => {
+        const course = "Next Level Web Development";
+        return Object.assign(Object.assign({}, student), { course });
+    };
+    const Student1 = addCourseToStudent({
+        name: "Mr Mir",
+        age: 24,
+        email: "mir@gmail.com",
+        devType: "Next Level Web Developer",
+    });
+    const Student2 = addCourseToStudent({
+        name: "Mr Hossain",
+        age: 24,
+        email: "mir@gmail.com",
+        hasWatch: true,
     });
     //
 }
--- src/2.5.ts
+++ src/2.5.ts
@@ -4,10 +4,11 @@
   const createArray = (param: string): string[] => {
     return [param];
   };
+  const result1 = createArray("Kabul");
+  //
   const createArrayWithGeneric = <T>(param: T): T[] => {
     return [param];
   };
-  const result1 = createArray("Habul");
   const result2 = createArrayWithGeneric<number>(12);
   const result3 = createArrayWithGeneric<{
     id: number;
@@ -19,5 +20,28 @@
     age: 19,
   });
 
+  //   // Function with Tuple
+
+  const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+    return [param1, param2];
+  };
+  const result4 = createArrayWithTuple<string, number>("Kabul", 11);
+
+  const addCourseToStudent = <T>(student: T) => {
+    const course = "Next Level Web Development";
+    return { ...student, course };
+  };
+  const Student1 = addCourseToStudent({
+    name: "Mr Mir",
+    age: 24,
+    email: "mir@gmail.com",
+    devType: "Next Level Web Developer",
+  });
+  const Student2 = addCourseToStudent({
+    name: "Mr Hossain",
+    age: 24,
+    email: "mir@gmail.com",
+    hasWatch: true,
+  });
   //
 }
