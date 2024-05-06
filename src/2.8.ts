95c3405fe1da24d354c9f49fb31c5e9a81b5974b
abdullah01 <arfathossen541@gmail.com>
2024-05-06 09:33:29 +0600

    Topic : Create a simple Promise Func

---
+++ src/2.8.ts
@@ -0,0 +1,25 @@
+{
+  // Basic Promise
+
+  interface Something {
+    something: string;
+  }
+  const createPromise = (): Promise<Something> => {
+    return new Promise<Something>((resolve, reject) => {
+      const data: Something = { something: "something" };
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Failed to load data");
+      }
+    });
+  };
+  // Calling create promise function
+  const showData = async (): Promise<Something> => {
+    const data: Something = await createPromise();
+    return data;
+  };
+  showData();
+  // Asynchronous
+  //
+}
